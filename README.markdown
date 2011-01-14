4be4272dfaa22f1b441d5e1e9ce5f878db35c0b1
DFischer <me@danielfischer.com>
2011-01-14 01:35:13 -0800

    Updated to latest standards

--- README.markdown
+++ README.markdown
@@ -1,3 +1,10 @@
+Clone, and fire up `ruby app.rb` and you should be good to go!
+
+
+# Contributors
+Daniel Fischer
+
+
 This borrows a lot of code from the two main Compass/Sinatra examples.
 
 http://github.com/chriseppstein/compass-sinatra
--- app.rb
+++ app.rb
@@ -14,17 +14,18 @@ set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, 'views'
 set :public, 'public'
+set :haml, {:format => :html5 } # Get with the program. Start using HTML5. Comment out if you're not ready.
 
 # Configure Compass
 configure do
-  Compass.configuration.parse(File.join(Sinatra::Application.root, 'config.rb'))
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config.rb'))
 end
 
 # At a minimum the main sass file must reside within the views directory
 # We create /views/stylesheets where all our sass files can safely reside
 get '/stylesheets/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
-  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
+  scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
 end
 
 get '/about' do

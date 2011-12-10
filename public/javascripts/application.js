7631027770851521d413cc3c3ec386fbb433cd0f
Jonny <jono.kang@gmail.com>
2011-12-10 02:41:12 -0800

    finished mostly everything and the aside

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
 
-	// Open external links in a new window
-	hostname = window.location.hostname
-	$("a[href^=http]")
-		.not("a[href*='" + hostname + "']")
-		.addClass('link external')
-		.attr('target', '_blank');
-	
+  // Open external links in a new window
+  hostname = window.location.hostname
+  $("a[href^=http]")
+    .not("a[href*='" + hostname + "']")
+    .addClass('link external')
+    .attr('target', '_blank');
+
+  $('a#nextpage').click(function(event) {
+    event.preventDefault;
+    $('section.firstPage').slideUp('slow', function(){
+      $('section.secondPage').show();
+    });
+  });
+
 });
+

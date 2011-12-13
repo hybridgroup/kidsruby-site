60ac15b6b0f67435ae6ddd5cd242ac493b7de195
Jonny <jono.kang@gmail.com>
2011-12-13 15:56:13 -0800

    added some api json for twitter and facebook counts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     event.preventDefault();
     $('section.firstPage').fadeOut('slow', function(){
       $('section.secondPage').fadeIn();
+      $(window).scrollTop(1);
     });
   });
 
@@ -18,8 +19,29 @@ $(document).ready(function() {
     event.preventDefault();
     $('section.secondPage').fadeOut('slow', function(){
       $('section.firstPage').fadeIn();
+      $(window).scrollTop(1);
     });
   });
+
+  fblink = 'http://api.ak.facebook.com/restserver.php?v=1.0&method=links.getStats&urls=www.kidsruby.com&format=json';
+
+  fbObject = $.getJSON(fblink);
+
+  $.getJSON(fblink, function(data) {
+      var items = [];
+
+      $.each(data, function(key, val) {
+        items.push('<li id="' + key + '">' + val + '</li>');
+      });
+
+      $('<ul/>', {
+        'class': 'my-new-list',
+        html: items.join('')
+      }).appendTo('section.firstPage');
+    });
+
+  twitterlink = 'http://urls.api.twitter.com/1/urls/count.json?url=www.kidsruby.com&callback=twttr.receiveCount'
+  twitterObject = $.getJSON(fblink);
 });
 
 

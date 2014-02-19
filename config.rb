f1e03fd87c504e0f0c8e6260ebb9de4fdc60372e
Luis Felipe Perez <slashcat@gmail.com>
2014-02-19 16:48:33 -0600

    integrate blog template

--- config.rb
+++ config.rb
@@ -1,10 +1,39 @@
 ###
-# Compass
+# Blog settings
 ###
 
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+###
+# Compass
+###
 
 # Change Compass configuration
 # compass_config do |config|
@@ -18,20 +47,19 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (http://middlemanapp.com/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -40,8 +68,14 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
+# Reload the browser automatically whenever files change
+# activate :livereload
+
 # Methods defined in the helpers block are available in templates
 # helpers do
+#   def some_helper
+#     "Helping"
+#   end
 # end
 
 set :css_dir, 'stylesheets'
@@ -52,32 +86,18 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  ignore 'images/*.psd'
-  ignore 'stylesheets/lib/*'
-  ignore 'stylesheets/vendor/*'
-  ignore 'javascripts/lib/*'
-  ignore 'javascripts/shims/*'
-
   # For example, change the Compass output style for deployment
-  activate :minify_css
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
 
   # Enable cache buster
-  # activate :cache_buster
+  # activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
 
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
   # Or use a different image path
-  # set :http_path, "/Content/images/"
-
-  # Enable blog
-  activate :blog
+  # set :http_prefix, "/Content/images/"
 end

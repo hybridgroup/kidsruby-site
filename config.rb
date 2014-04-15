236377f873c996d1a53bfc971f2d20748a19847b
nathan <nathanzankich@gmail.com>
2014-04-15 11:36:55 -0700

    Made blog title link

--- config.rb
+++ config.rb
@@ -7,13 +7,12 @@
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
-  blog.permalink = "{year}/{month}/{day}.html"
+  blog.permalink = "{year}-{month}-{day}-{title}.html"
   # Matcher for blog source files
     blog.sources = "articles/{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
     blog.layout = "layouts/layout"
   # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
